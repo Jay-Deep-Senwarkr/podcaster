04b22145f0806b7eae0cdc0daad82f46636dcec3
Jaydeep <jdsenwarkr@gmail.com>
2024-06-28 12:40:17 +0530

    28-06-12:40 pm

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+// https://capable-mouse-64.clerk.accounts.dev
+const authConfig = {
+    providers: [
+      {
+        domain: "https://capable-mouse-64.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
